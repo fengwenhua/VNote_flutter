2a06244e4557bf71578b54817ad764a4c98b6047
fengwenhua <13612265462@163.com>
2019-12-12 22:10:20 +0800

    打算开始写document生成List

---
+++ lib/utils/document_list_util.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:vnote/models/document_model.dart';
+
+class DocumentListUtil{
+  static List<Document> getDirectoryList(String data){
+      List<Document> result;
+      // 先拿到json, 应该先从本地拿, 再从网络拿, 看情况
+
+      // 解析json
+
+      // 获取所有的路径
+
+      // List排序
+
+      // 遍历生成result
+
+      // 写到本地
+
+      return result;
+  }
+
+  static List<Document> getAllList(){
+    List<Document> result;
+
+    return result;
+  }
+}
